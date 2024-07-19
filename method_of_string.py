d34c696cd90382b7960c754237b2d2e5eb530e17
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-19 23:56:07 +0530

    Create method_of_string.py

---
+++ method_of_string.py
@@ -0,0 +1,31 @@
+a="!!!!!harry !!!!!!!!! harry!!!!!"
+print(len(a))
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("harry","shristi"))
+print(a.split(" "))
+#capitalize 
+b="introduction of python "
+print(b.capitalize())
+print(len(b))
+print(b.center(10))
+c="hun hun hun hun hun hun"
+print(c.count("hun"))
+#endswith()
+d="shristi is a poor coder!!!"
+print(d.endswith("!"))
+e="!!python is also knows as programminh lanuage!!"
+print(e.find("is"))
+#isalmum()
+f="welcome"
+print(f.isalnum())
+#isalpha()
+g="fuvk8"
+h="fucku"
+print(g.isalpha())
+print(h.isalpha())
+#islower()
+#in this string we store any string in any small alhapbet only.
+i="hellow sd"
+print(i.islower())
