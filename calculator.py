4333e9f704fd860fc573658a3eff4d708cd89a76
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-19 23:14:34 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,20 @@
+#code-03
+#create a calculator in py
+#declaring variables
+#by using user input 
+a=int(input("enter your first number:"))
+b=int(input("enter your second number:"))
+#addition (a,b)
+print("The sum of first number and second number",a+b)
+#subtraction(a,b)
+print("The sum of firstnumber and second number",a-b)
+#multiplication(a,b)
+print("The sum of first number and second number",a*b)
+#division(a,b)
+print("The sum of first number and second number",a/b)
+#module(a,b)
+print("The sum of first number and second number",a%b)
+#floor division(a,b)
+print("The sum of first number and second number",a//b)
+#exponential(a,b)
+print("The sum of first number and second number",a**b)
