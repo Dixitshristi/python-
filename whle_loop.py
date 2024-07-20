ff3a626ae85a3b15f8fd1e73223b7c583dac6778
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-20 15:36:44 +0530

    Create whle_loop.py

---
+++ whle_loop.py
@@ -0,0 +1,12 @@
+#increment operator using while loop 
+i=0
+while(i<3):
+    print(i)
+    i=i+1
+   
+print("GAP !")
+#decrement operator using while loop
+c=5
+while(c>1):
+    print(c)
+    c=c-1
