bf696a331635c2e84b5199449b440333d3c0b82a
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-20 15:25:18 +0530

    Create table_for_loop.py

---
+++ table_for_loop.py
@@ -0,0 +1,9 @@
+#create a table of any number 
+#by using for loop
+#by using user input 
+  
+num=int(input("enter your number:"))
+
+for i in range(1,11):
+    print(num,'x',i,'=',num*i)
+print("TABLE IS SUCESSFULLY .....")
