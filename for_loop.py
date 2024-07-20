29099f6bebfe9d3ecbc5438293f6d8fe6379960a
Shristi Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-07-20 15:13:16 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,22 @@
+#separte each other
+name = 'neti'
+for i in name:
+  print(i)
+#colour 
+colour=["red","green","yellow","grey","pink","blue"]
+for j in colour:
+    print(j)
+print("done it")
+#range
+for k in range(5):
+  print(k)
+print("got it!")
+
+for n in range(5):
+  print(n+1)
+print("got it!")
+
+
+#by counting range
+for l in range(1,6):
+  print(l)
