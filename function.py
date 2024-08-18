19cfbf7588f9e7a66885c005213195f70accc107
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2024-08-18 15:52:02 +0530

    function.py

---
+++ function.py
@@ -0,0 +1,18 @@
+def calculationgmean (a,b):
+    mean=(a*b)/(a+b)
+    print(mean)   
+def isgreater (a,b):
+    if(a>b):
+      print("first number is greater than second number:")
+    else:
+      print("second number is freater than first number and both the number is equal to:")
+def issmaller(a,b):
+    pass
+a=5
+b=3
+isgreater(a,b)
+calculationgmean(a,b)
+c=7
+d=6
+isgreater(c,d)
+calculationgmean(c,d)
