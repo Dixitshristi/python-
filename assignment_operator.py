b9a3359653020b17758f8e41919f90b19333255f
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-10-01 15:35:49 +0530

    assignment_operator.py

---
+++ assignment_operator.py
@@ -0,0 +1,32 @@
+print("Assignment operator")
+print("Simple assignment operator")
+a = 8
+print(a)
+name = 'riya'
+print(name)
+print("Arguments assignment operator")
+print("add")
+b = 5
+b += 5
+print(b)
+print("sub")
+c = 10
+c -=5
+print(c)
+print("multiply")
+d = 15
+d *=5
+print(d)
+print("divide")
+e = 5
+e /= 5
+print(e)
+print("module")
+f = 45
+f %= 5
+print(f)
+print("floor operator")
+g = 3
+i = 5
+g //= i 
+print(g)
