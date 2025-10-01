1edbad33fc85f91a63305bd4d8a7fad354e4a023
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-10-01 14:56:36 +0530

    arithmetic_operators.py

---
+++ arithmetic_operators.py
@@ -0,0 +1,8 @@
+print("Arithmetic operators")
+a = 6
+b = 4
+print("The addition of a operator and b operator is",a+b)
+print("The subtraction of a operator and b operator is",a-b)
+print("The multiplication of a operator and b operator is",a*b)
+print("The division of a operator and b operator is",a/b)
+
