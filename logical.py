439da06455c81ca277e57f4e6abe872096101bb3
Shristi-Dixit <140090682+Dixitshristi@users.noreply.github.com>
2025-10-21 18:12:28 +0530

    logical.py

---
+++ logical.py
@@ -0,0 +1,20 @@
+a = True or False
+
+#TRUTH TABLE OF 'OR'
+
+print("True or False is",True or False)
+print("True or True is",True or True)
+print("False or True is",False or False)
+print("False or False is",False or False)
+
+#TRUTH TABLE OF 'and'
+
+print("True  and False is",True  and False)
+print("True  and True is",True  and True)
+print("False  and True is",False  and False)
+print("False and False is",False  and False)
+
+#TRUTH TABLE OF 'NOT'
+
+print(not(False))
+print(not(True))
